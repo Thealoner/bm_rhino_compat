d4cc000afedc265107aff9f4f04ca051ab19382f
Eduard Nikolaiev <e.nikolaiev@astoundcommerce.com>
2022-04-24 15:23:57 +0300

    Show compatibilty mode

--- cartridges/bm_rhino_compat/cartridge/controllers/RhinoCompat.js
+++ cartridges/bm_rhino_compat/cartridge/controllers/RhinoCompat.js
@@ -2,14 +2,19 @@
 var server = require('server');
 
 server.get('Show', function (req, res, next) {
+    var System = require('dw/system/System');
     var testRhino = require('*/cartridge/scripts/rhino/testRhino');
 
+    var rawCompatibilityMode = System.getCompatibilityMode();
+    var compatibilityMode = (rawCompatibilityMode / 100).toFixed(0) + '.' + (rawCompatibilityMode % 100).toString();
     var testResults = testRhino.getTestResults();
 
     res.render('rhinoCompat/rhinoFancy', {
+        compatibilityMode: compatibilityMode,
         results: testResults.results,
         summary: testResults.summary
     });
+
     next();
 });
 
